7a307f585ddc9780bd3a11935b3d27d73bfe7a6f
Kevin Ai <kevin.ai@nordicsemi.no>
2021-08-07 16:38:24 +0800

    put overlay in boards folder

---
+++ tfm_hello/README.rst
@@ -0,0 +1,60 @@
+.. _tfm_hello_world:
+
+TF-M Hello World
+################
+
+.. contents::
+   :local:
+   :depth: 2
+
+A simple sample based on Hello World that demonstrates adding Trusted Firmware-M (TF-M) to an application.
+
+Overview
+********
+
+This sample uses the Platform Security Architecture (PSA) API to calculate a SHA-256 digest and the TF-M platform read service to read two FICR registers.
+The PSA API call is handled by the TF-M secure firmware.
+
+Requirements
+************
+
+The sample supports the following development kits:
+
+.. table-from-rows:: /includes/sample_board_rows.txt
+   :header: heading
+   :rows: nrf5340dk_nrf5340_cpuappns, nrf9160dk_nrf9160ns
+
+Building and Running
+********************
+
+.. |sample path| replace:: :file:`samples/tfm/tfm_hello_world`
+
+.. include:: /includes/build_and_run.txt
+
+Testing
+=======
+
+After programming the sample, the following output is displayed in the console:
+
+.. code-block:: console
+
+    Hello World! nrf5340dk_nrf5340_cpuapp
+    Generating random number
+    0x8aefe6b7473f2e2c170bbd3eb39aa7679bc1e7693a11030b0a4c1c8ba41eb457
+    Reading some secure memory that NS is allowed to read
+    FICR->INFO.PART: 0x00005340
+    FICR->INFO.VARIANT: 0x514b4141
+    Hashing 'Hello World! nrf5340dk_nrf5340_cpuapp'
+    SHA256 digest:
+    0x12f0c84eecba8497cc0bec1ebc5a785df2ae027a2545921d6cdc0920c5aaefd7
+
+Dependencies
+*************
+
+This sample uses the TF-M module that can be found in the following location in the |NCS| folder structure:
+
+* ``modules/tee/tfm/``
+
+This sample uses the following libraries:
+
+* :ref:`lib_tfm_ioctl_api`
---
+++ tfm_hello/src/main.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <string.h>
+#include <stdio.h>
+#include <tfm_ns_interface.h>
+#include "psa/crypto.h"
+#include <tfm/tfm_ioctl_api.h>
+#include <pm_config.h>
+
+#define HELLO_PATTERN "Hello World! %s"
+
+static uint32_t secure_read_word(intptr_t ptr)
+{
+	uint32_t err = 0;
+	uint32_t val;
+	enum tfm_platform_err_t plt_err;
+
+	plt_err = tfm_platform_mem_read(&val, ptr, 4, &err);
+	if (plt_err != TFM_PLATFORM_ERR_SUCCESS || err != 0) {
+		printk("tfm_..._mem_read failed: plt_err: 0x%x, err: 0x%x\n",
+			plt_err, err);
+		return -1;
+	}
+
+	return val;
+}
+
+static void print_hex_number(uint8_t *num, size_t len)
+{
+	printk("0x");
+	for (int i = 0; i < len; i++) {
+		printk("%02x", num[i]);
+	}
+	printk("\n");
+}
+
+void main(void)
+{
+	char hello_string[sizeof(HELLO_PATTERN) + sizeof(CONFIG_BOARD)];
+	char hello_digest[32];
+	uint8_t random_bytes[32];
+	size_t len;
+	psa_status_t status;
+
+	len = snprintf(hello_string, sizeof(hello_string),
+		HELLO_PATTERN, CONFIG_BOARD);
+
+	printk("%s\n", hello_string);
+
+	printk("Generating random number\n");
+	status = psa_generate_random(random_bytes, sizeof(random_bytes));
+	if (status != PSA_SUCCESS) {
+		printk("psa_generate_random failed with status %d\n", status);
+	} else {
+		print_hex_number(random_bytes, sizeof(random_bytes));
+	}
+
+	printk("Reading some secure memory that NS is allowed to read\n");
+	printk("FICR->INFO.PART: 0x%08x\n",
+		secure_read_word((intptr_t)&NRF_FICR_S->INFO.PART));
+	printk("FICR->INFO.VARIANT: 0x%08x\n",
+		secure_read_word((intptr_t)&NRF_FICR_S->INFO.VARIANT));
+
+	printk("Hashing '%s'\n", hello_string);
+	status = psa_crypto_init();
+	if (status != PSA_SUCCESS) {
+		printk("psa_crypto_init failed with status %d\n", status);
+	}
+	status = psa_hash_compute(PSA_ALG_SHA_256, hello_string,
+					strlen(hello_string), hello_digest,
+					sizeof(hello_digest), &len);
+
+	if (status != PSA_SUCCESS) {
+		printk("psa_hash_compute failed with status %d\n", status);
+	} else {
+		printk("SHA256 digest:\n");
+		print_hex_number(hello_digest, 32);
+	}
+}
