4df8f36b52391953c2b07dcfd798b4a4dca44012
Kevin Ai <kevin.ai@nordicsemi.no>
2022-07-09 17:47:12 +0800

    update app core and net core together

--- ble_ipc/netcore_ipc/src/main.c
+++ ble_ipc/netcore_ipc/src/main.c
@@ -43,7 +43,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 static K_SEM_DEFINE(ble_conn_ok, 0, 1);
 
 struct bt_conn *current_conn;
-// static struct bt_conn *auth_conn;
+static struct bt_conn *auth_conn;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -139,33 +139,6 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
 }
 
-#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
-static void security_changed(struct bt_conn *conn, bt_security_t level,
-			     enum bt_security_err err)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	if (!err) {
-		LOG_INF("Security changed: %s level %u", log_strdup(addr),
-			level);
-	} else {
-		LOG_WRN("Security failed: %s level %u err %d", log_strdup(addr),
-			level, err);
-	}
-}
-#endif
-
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected    = connected,
-	.disconnected = disconnected,
-#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
-	.security_changed = security_changed,
-#endif	
-};
-
-
 static void bt_nus_receive_cb(struct bt_conn *conn, const uint8_t *const data,
 			  uint16_t len)
 {	
@@ -227,6 +200,54 @@ void setup_ext_int(void)
 	}
 }
 
+static int send_to_app(void)
+{	
+	int ret;
+	static uint8_t cnt;
+	char test_str[20];
+
+	test_str[0] = 0x1;
+	snprintf(&test_str[1], 16, "I am from NET %c", cnt++);
+	ret = net2app_test(test_str, 16);
+	if (ret)
+	{
+		LOG_ERR("ipc test error %d", ret);
+	}
+	else
+	{
+		LOG_DBG("ipc test done %x", cnt-1);
+	}
+
+	return ret;	
+}
+
+
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+static void security_changed(struct bt_conn *conn, bt_security_t level,
+			     enum bt_security_err err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (!err) {
+		LOG_INF("Security changed: %s level %u", log_strdup(addr),
+			level);
+	} else {
+		LOG_WRN("Security failed: %s level %u err %d", log_strdup(addr),
+			level, err);
+	}
+}
+#endif
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected    = connected,
+	.disconnected = disconnected,
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+	.security_changed = security_changed,
+#endif
+};
+
 #if defined(CONFIG_BT_NUS_SECURITY_ENABLED)
 static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
 {
@@ -241,7 +262,7 @@ static void auth_passkey_confirm(struct bt_conn *conn, unsigned int passkey)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
 
-	// auth_conn = bt_conn_ref(conn);
+	auth_conn = bt_conn_ref(conn);
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
@@ -285,32 +306,46 @@ static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
 static struct bt_conn_auth_cb conn_auth_callbacks = {
 	.passkey_display = NULL, //auth_passkey_display,
 	.passkey_confirm = NULL, //auth_passkey_confirm,
-	.cancel = NULL, //auth_cancel,	
+	.cancel = auth_cancel,
+};
+
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
 	.pairing_complete = pairing_complete,
 	.pairing_failed = pairing_failed
 };
+
+// static void num_comp_reply(bool accept)
+// {
+// 	if (accept) {
+// 		bt_conn_auth_passkey_confirm(auth_conn);
+// 		LOG_INF("Numeric Match, conn %p", (void *)auth_conn);
+// 	} else {
+// 		bt_conn_auth_cancel(auth_conn);
+// 		LOG_INF("Numeric Reject, conn %p", (void *)auth_conn);
+// 	}
+
+// 	bt_conn_unref(auth_conn);
+// 	auth_conn = NULL;
+// }
+
+// void button_changed(uint32_t button_state, uint32_t has_changed)
+// {
+// 	uint32_t buttons = button_state & has_changed;
+
+// 	if (auth_conn) {
+// 		if (buttons & KEY_PASSKEY_ACCEPT) {
+// 			num_comp_reply(true);
+// 		}
+
+// 		if (buttons & KEY_PASSKEY_REJECT) {
+// 			num_comp_reply(false);
+// 		}
+// 	}
+// }
+
+#else
+static struct bt_conn_auth_cb conn_auth_callbacks;
 #endif
-
-static int send_to_app(void)
-{	
-	int ret;
-	static uint8_t cnt;
-	char test_str[20];
-
-	test_str[0] = 0x1;
-	snprintf(&test_str[1], 16, "I am from NET %c", cnt++);
-	ret = net2app_test(test_str, 16);
-	if (ret)
-	{
-		LOG_ERR("ipc test error %d", ret);
-	}
-	else
-	{
-		LOG_DBG("ipc test done %x", cnt-1);
-	}
-
-	return ret;	
-}
 
 void main(void)
 {
@@ -319,11 +354,21 @@ void main(void)
 	
 	LOG_INF("### netcore firmware compiled at %s %s", __TIME__, __DATE__);
 	
-	setup_ext_int();	
+	setup_ext_int();
 
-#ifdef 	CONFIG_BT_NUS_SECURITY_ENABLED
-	bt_conn_auth_cb_register(&conn_auth_callbacks);		
-#endif	
+	if (IS_ENABLED(CONFIG_BT_NUS_SECURITY_ENABLED)) {
+		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
+		if (err) {
+			printk("Failed to register authorization callbacks.\n");
+			return;
+		}
+
+		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+		if (err) {
+			printk("Failed to register authorization info callbacks.\n");
+			return;
+		}
+	}
 
 	err = bt_enable(NULL);
 	if (err) {
--- ble_ipc/uart_nrf_dfu/program.bat
+++ ble_ipc/uart_nrf_dfu/program.bat
@@ -1,5 +1,5 @@
-::nrfjprog -f NRF53 --coprocessor CP_NETWORK --recover
-::nrfjprog -f NRF53 --recover
+nrfjprog -f NRF53 --coprocessor CP_NETWORK --recover
+nrfjprog -f NRF53 --recover
 ::west flash -d build_nrf5340dk_nrf5340_cpuapp\hci_rpmsg
 nrfjprog -f NRF53 --coprocessor CP_NETWORK --eraseall
 ::nrfjprog -f NRF53 --coprocessor CP_NETWORK --program build_nrf5340dk_nrf5340_cpuapp/hci_rpmsg/zephyr/merged_CPUNET.hex --verify
